5385ba07d148b3530531838f84634f338a8ad190
keeper62 <bas.vanderveer@outlook.com>
2025-04-13 18:32:11 +0200

    removed even more overhead

--- dataset_class/abstract_dataset.py
+++ dataset_class/abstract_dataset.py
@@ -9,43 +9,34 @@ class AbstractBGLDataset(Dataset, ABC):
         self.prediction_steps = prediction_steps
         self.context_length = context_length
         self.test_mode = test_mode
-        
+
         if self.test_mode:
             self.data = self._read_data_training(path)
         else:
             self.data = self._read_data(path)
 
-        # Unpack and store tensors separately for speed
-        data, timestamps = zip(*self.data)
-        self.tokens = list(data)  # still raw strings or transformed later
-        self.timestamps = torch.tensor(timestamps, dtype=torch.float32)
-        
-        self.num_lines = len(self.tokens)
+        # Preprocess once here
+        if self.transform:
+            self.data = [(self.transform(log), ts) for log, ts in self.data]
 
-    @abstractmethod
-    def _read_data(self, path):
-        pass
-
-    @abstractmethod
-    def _read_data_training(self, path):
-        pass
+        self.num_lines = len(self.data)
 
     def __len__(self):
         return max(0, self.num_lines - self.context_length - self.prediction_steps + 1)
 
     def __getitem__(self, idx):
-        input_window = self.tokens[idx: idx + self.context_length]
-        output = self.tokens[idx + self.context_length + 1]
-        output_window = input_window[1:] + [output]
+        data, timestamps = zip(*self.data)
 
-        if self.transform:
-            input_window = [self.transform(log) for log in input_window]
-            output_window = [self.transform(log) for log in output_window]
+        input_window = data[idx: idx + self.context_length]
+        output = data[idx + self.context_length + 1]
+
+        output_window = list(input_window[1:]) + [output]
+        input_timestamps = timestamps[idx: idx + self.context_length]
 
         input_window = torch.tensor(input_window, dtype=torch.long)
         output_window = torch.tensor(output_window, dtype=torch.long)
-
-        input_timestamps = self.timestamps[idx: idx + self.context_length]
+        input_timestamps = torch.tensor(input_timestamps, dtype=torch.float32)
 
         return input_window, output_window, input_timestamps
+
 
