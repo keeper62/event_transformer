d9e1510880758afd9a3a85f103c1ce9cfc45c39d
keeper62 <bas.vanderveer@outlook.com>
2025-05-09 16:07:41 +0200

    dfgfd

--- training/train.py
+++ training/train.py
@@ -587,8 +587,8 @@ class TransformerLightning(pl.LightningModule):
         
         # Update important class metrics
         if len(self.important_classes) > 0:
-            self._logger.debug(f"Final devices - targets: {targets.device}, important_classes: {self.important_classes.device}")
             self.important_classes.to(targets.device)
+            self._logger.debug(f"Final devices - targets: {targets.device}, important_classes: {self.important_classes.device}")
             important_mask = torch.isin(targets, self.important_classes)
             self._logger.debug("Test 3")
             if important_mask.any():
